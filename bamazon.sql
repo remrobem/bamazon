aca515490d48df37e810e265dc0e118b16519635
remrobem <rob.martin.5758@gmail.com>
2018-05-07 09:15:20 -0400

    database

--- bamazon.sql
+++ bamazon.sql
@@ -10,7 +10,7 @@ USE bamazon;
 CREATE TABLE products (
  
   item_id INTEGER(10) not null auto_increment,
-  product_name VARCHAR(100) NOT NULL,
+  product_name VARCHAR(100) NOT NULL UNIQUE,
   department_name VARCHAR(100) NOT NULL,
   price decimal(10,2)  NOT NULL,
   stock_quantity integer(10)  NOT NULL,
@@ -18,16 +18,3 @@ CREATE TABLE products (
    PRIMARY KEY(item_id)
  
 );
-
-INSERT INTO products (product_name, department_name, price, stock_quantity) 
-VALUES ('couch - 2 cushion leather', 'living room', 899.99, 12);
-INSERT INTO products (product_name, department_name, price, stock_quantity) 
-VALUES ('couch - 3 cushion leather', 'living room', 1299.99, 7);
-INSERT INTO products (product_name, department_name, price, stock_quantity) 
-VALUES ('table - kitchen', 'kitchen', 499.99, 8);
-INSERT INTO products (product_name, department_name, price, stock_quantity) 
-VALUES ('chair - kitchen tabe', 'kitchen', 49.99, 24);
-INSERT INTO products (product_name, department_name, price, stock_quantity) 
-VALUES ('desk - student', 'office', 399.99, 14);
-
-
---
+++ bamazon_seed.sql
@@ -0,0 +1,53 @@
+-- create data from the bamazon database --
+
+-- Makes it so all of the following code will affect this database --
+USE bamazon;
+
+-- create data from the products table in the bamazon database --
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('couch - 2 cushion leather', 'living room', 899.99, 12);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('couch - 3 cushion leather', 'living room', 1299.99, 7);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('table - kitchen', 'kitchen', 499.99, 8);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('chair - kitchen tabe', 'kitchen', 49.99, 24);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('desk - student', 'office', 399.99, 11);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('desk - business', 'office', 599.99, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('notebook - 3 subject 300 pages', 'paper', 3.99, 79);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('notebook - 5 subject 500 pages', 'paper', 9.99, 56);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('pen - blue', 'supplies', 0.99, 516);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('pen - black', 'supplies', 0.99, 516);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('pen - red', 'supplies', 0.99, 516);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('couch - 4 cushion leather', 'living room', 899.99, 12);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('couch - 5 cushion leather', 'living room', 1299.99, 7);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('table - kitchen long', 'kitchen', 499.99, 8);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('chair - kitchen table wide', 'kitchen', 49.99, 24);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('desk - student wide', 'office', 399.99, 11);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('desk - business glass', 'office', 599.99, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('notebook - binder 3 subject 300 pages', 'paper', 3.99, 79);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('notebook - binder 5 subject 500 pages', 'paper', 9.99, 56);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('pen - blue click', 'supplies', 0.99, 516);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('pen - black click', 'supplies', 0.99, 516);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('pen - red click', 'supplies', 0.99, 516);
+
+
