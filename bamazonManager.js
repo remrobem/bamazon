f1595db26b516e03c095c0ecb19406bb6b1ba705
remrobem <rob.martin.5758@gmail.com>
2018-05-08 18:50:37 -0400

    manager view processes all requests

--- bamazonManager.js
+++ bamazonManager.js
@@ -95,7 +95,7 @@ function viewInventory() {
         name: "lowInventoryQuantity",
         type: "prompt",
         message: "Show products with a stock quantity less than:",
-        validate: ValidateNumeric,
+        validate: VValidatePositive,
     }];
 
 
@@ -125,49 +125,84 @@ function viewInventory() {
 };
 
 function addInventory() {
+
     const addInventoryItem = [{
             name: "addInventoryItem",
             type: "prompt",
             message: "What item number:",
-            validate: ValidateNumeric,
+            validate: ValidatePositive
         },
         {
             name: "addInventoryQuantity",
             type: "prompt",
-            message: "How much invntory to add:",
-            validate: ValidateNumeric,
+            message: "How much inventory to add:",
+            validate: ValidatePositive,
         }
-
     ];
+
     inquirer.prompt(addInventoryItem)
-    .then(answer => {
-
-        // connection.query(query, [answer.lowInventoryQuantity], function (err, res) {
-        //     if (err) throw err;
-
-        //     if (res.length) {
-        //         res.forEach(product => {
-        //             inventoryArr.push([product.item_id, product.product_name, product.stock_quantity])
-        //         });
-        //         console.log(inventoryArr.toString());
-        //         anotherActivity()
-        //     };
-        // });
-    });
+        .then(answer => {
+            query = "UPDATE products SET stock_quantity = stock_quantity + ? where item_id = ?";
+            connection.query(query, [answer.addInventoryQuantity, answer.addInventoryItem], function (err, res) {
+                if (err) throw err;
+                // expect one row to be updated
+                if (res.changedRows === 1) {
+                    console.log(`The inventory has been updated`)
+                } else {
+                    console.log(`Something went wrong. Rows updated: ${res.affected_rows}`)
+                }
+                anotherActivity()
+            });
+        });
 
 
 };
 
 function addProduct() {
 
+    const addProduct = [{
+            name: "addProductName",
+            type: "prompt",
+            message: "Enter Product Name:"
+        },
+        {
+            name: "addProductDept",
+            type: "prompt",
+            message: "Enter Department Name:"
+        },
+        {
+            name: "addProductPrice",
+            type: "prompt",
+            message: "Enter Price:",
+            validate: ValidatePositive,
+        },
+        {
+            name: "addProductQuantity",
+            type: "prompt",
+            message: "Enter Stock Quantity:",
+            validate: ValidatePositive,
+        }
+    ];
+
+
+    inquirer.prompt(addProduct)
+        .then(answer => {
+            query = "INSERT INTO products (product_name, department_name, price, stock_quantity) values( ? , ? , ? , ?) ";
+            connection.query(query, [answer.addProductName, answer.addProductDept, answer.addProductPrice, answer.addProductQuantity], function (err, res) {
+                if (err) throw err;
+                // expect one row to be added
+                if (res.affectedRows === 1) {
+                    console.log(`Your product has been added and assigned item id ${res.insertId}`)
+                } else {
+                    console.log(`Something went wrong. Rows updated: ${res.affected_rows}`)
+                }
+                anotherActivity()
+            });
+        });
+
+
 
 };
-
-
-
-
-
-
 
 function anotherActivity() {
     inquirer.prompt(anotherActivityQuestion).then(answers => {
@@ -183,4 +218,12 @@ function anotherActivity() {
 
 function ValidateNumeric(value) {
     return !isNaN(value)
+};
+
+function ValidatePositive(value) {
+    if (value > 0 && !isNaN(value)) {
+        return true;
+    } else {
+        return false;
+    };
 };
